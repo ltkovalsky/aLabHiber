c8027d41bd18895a801881084eeae053d53ab411
akovalsky <a@>
2023-05-13 18:11:23 +0600

    Initial commit

---
+++ src/main/java/com/example/alabhiber/ALabHiberApplication.java
@@ -0,0 +1,18 @@
+package com.example.alabhiber;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALabHiberApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ALabHiberApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // todo: call smth
+    }
+}
---
+++ src/test/java/com/example/alabhiber/ALabHiberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alabhiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ALabHiberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
